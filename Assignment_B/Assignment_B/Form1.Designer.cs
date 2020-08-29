96a89aa579edd55535fba8ec95c96e574826c5f1
mahadi0007 <mahadimahadi.hasan96@gmail.com>
2020-08-29 23:51:49 +0600

    functionality done

--- Assignment_B/Assignment_B/Form1.Designer.cs
+++ Assignment_B/Assignment_B/Form1.Designer.cs
@@ -29,24 +29,26 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.amountTaka = new System.Windows.Forms.TextBox();
+            this.convertToCombo = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.convert_btn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.error = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox2);
-            this.groupBox1.Controls.Add(this.textBox1);
-            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.error);
+            this.groupBox1.Controls.Add(this.textBox_Result);
+            this.groupBox1.Controls.Add(this.amountTaka);
+            this.groupBox1.Controls.Add(this.convertToCombo);
             this.groupBox1.Controls.Add(this.button2);
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.convert_btn);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
@@ -59,42 +61,39 @@
             this.groupBox1.Text = "Currency Converter";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
-            // label1
+            // textBox_Result
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 66);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(217, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Enter amount (in Taka) ";
+            this.textBox_Result.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox_Result.Location = new System.Drawing.Point(64, 264);
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.ReadOnly = true;
+            this.textBox_Result.Size = new System.Drawing.Size(491, 26);
+            this.textBox_Result.TabIndex = 8;
+            this.textBox_Result.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
-            // label2
+            // amountTaka
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(59, 119);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(110, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Convert To";
+            this.amountTaka.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.amountTaka.Location = new System.Drawing.Point(307, 68);
+            this.amountTaka.Name = "amountTaka";
+            this.amountTaka.Size = new System.Drawing.Size(248, 26);
+            this.amountTaka.TabIndex = 7;
             // 
-            // label3
+            // convertToCombo
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(228, 223);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 25);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "Converted Value";
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(480, 335);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Convert";
-            this.button1.UseVisualStyleBackColor = true;
+            this.convertToCombo.AllowDrop = true;
+            this.convertToCombo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.convertToCombo.FormattingEnabled = true;
+            this.convertToCombo.Items.AddRange(new object[] {
+            "Rupee",
+            "USD",
+            "Euro",
+            "Canadian dollar",
+            "Austrailan dollar"});
+            this.convertToCombo.Location = new System.Drawing.Point(307, 116);
+            this.convertToCombo.Name = "convertToCombo";
+            this.convertToCombo.Size = new System.Drawing.Size(248, 28);
+            this.convertToCombo.TabIndex = 6;
             // 
             // button2
             // 
@@ -105,39 +104,53 @@
             this.button2.TabIndex = 4;
             this.button2.Text = "Reset";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.resetBtn_Click);
             // 
-            // comboBox1
+            // convert_btn
             // 
-            this.comboBox1.AllowDrop = true;
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Rupee",
-            "USD",
-            "Euro",
-            "Canadian dollar",
-            "Austrailan dollar"});
-            this.comboBox1.Location = new System.Drawing.Point(307, 116);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(248, 28);
-            this.comboBox1.TabIndex = 6;
+            this.convert_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.convert_btn.Location = new System.Drawing.Point(480, 335);
+            this.convert_btn.Name = "convert_btn";
+            this.convert_btn.Size = new System.Drawing.Size(75, 23);
+            this.convert_btn.TabIndex = 3;
+            this.convert_btn.Text = "Convert";
+            this.convert_btn.UseVisualStyleBackColor = true;
+            this.convert_btn.Click += new System.EventHandler(this.convert_btn_click);
             // 
-            // textBox1
+            // label3
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox1.Location = new System.Drawing.Point(307, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(248, 26);
-            this.textBox1.TabIndex = 7;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(159, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Converted Value";
             // 
-            // textBox2
+            // label2
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.textBox2.Location = new System.Drawing.Point(64, 264);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(491, 26);
-            this.textBox2.TabIndex = 8;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Convert To";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter amount (in Taka) ";
+            // 
+            // error
+            // 
+            this.error.AutoSize = true;
+            this.error.Location = new System.Drawing.Point(175, 307);
+            this.error.Name = "error";
+            this.error.Size = new System.Drawing.Size(0, 25);
+            this.error.TabIndex = 9;
             // 
             // Form1
             // 
@@ -157,13 +170,14 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button convert_btn;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox convertToCombo;
+        private System.Windows.Forms.TextBox textBox_Result;
+        private System.Windows.Forms.TextBox amountTaka;
+        private System.Windows.Forms.Label error;
     }
 }
 
--- Assignment_B/Assignment_B/Form1.cs
+++ Assignment_B/Assignment_B/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -23,6 +24,96 @@ namespace Assignment_B
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void convert_btn_click(object sender, EventArgs e)
+        {
+
+            if (amountTaka.Text == "" || convertToCombo.SelectedItem == null)
+            {
+
+                error.Text = "Please check all inputs";   //error message for null inputs
+            }
+
+            else
+            {
+                error.Text = "";
+
+                int inputAmount = int.Parse(amountTaka.Text);    //input Taka
+
+                string convertTo = (string)convertToCombo.SelectedItem;  //input convert to
+
+                double convertedAmount = 0.00;
+
+
+
+
+
+                switch (convertTo)
+                {
+                    case "Rupee":
+
+                        convertedAmount = inputAmount * 0.862361;
+
+
+                        break;
+                    case "USD":
+                        convertedAmount = inputAmount * 0.0117905;
+
+                        break;
+
+                    case "Euro":
+
+                        convertedAmount = inputAmount * 0.00990342;
+
+                        break;
+
+                    case "Canadian dollar":
+                        convertedAmount = inputAmount * 0.0154433;
+
+                        break;
+
+                    case "Austrailan dollar":
+                        convertedAmount = inputAmount * 0.0160038;
+
+                        break;
+                }
+
+
+
+
+
+
+                textBox_Result.Text = convertedAmount.ToString();  //update result in front end
+
+
+
+            }
+        }
+
+
+        //reset input fields
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            amountTaka.Text = "";
+            convertToCombo.Text = null;
+            textBox_Result.Text = "";
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void amountTaka_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
